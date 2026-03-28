7bd1ef1c03b681eb6d76b5747cfde2d11b41d913
Greyw0rks <Elchapidave@gmail.com>
2026-03-28 21:10:04 +0100

    Create index.ts to export core modules and utilities

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Core protocol logic
+export * from './lib';
+
+// Utilities
+export * from './utils';
+
+// Named exports (clean + explicit)
+export * from './constants';
+
+// If these have default exports
+export { default as api } from './api';
+export { default as wallet } from './wallet';
