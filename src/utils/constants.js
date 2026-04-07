e3cc9dec39d84c9d1527c0550512c05a26973ed5
Greyw0rks <elchapidave@gmail.com>
2026-04-07 12:23:50 +0100

    Add app constants: centralized config for contract addresses and network values

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const NETWORK = "mainnet";
+export const CONTRACT_ADDRESS = "SP1SY1E599GN04XRD2DQBKV7E62HYBJR2CT9S5QKK";
+export const CONTRACTS = {
+  CORE:         `${CONTRACT_ADDRESS}.proofleger3`,
+  CREDENTIALS:  `${CONTRACT_ADDRESS}.credentials`,
+  ACHIEVEMENTS: `${CONTRACT_ADDRESS}.achievements`,
+  ENDORSEMENTS: `${CONTRACT_ADDRESS}.endorsements`,
+  PROFILES:     `${CONTRACT_ADDRESS}.profiles`,
+};
+export const API_BASE = "https://api.hiro.so";
+export const EXPLORER = "https://explorer.hiro.so";
+export const STACKS_BLOCK_TIME_S = 600;
+export const MIN_FEE_MICROSTX = 1000;
+export const DOC_TYPES = ["diploma","certificate","research","art","contribution","award","other"];
